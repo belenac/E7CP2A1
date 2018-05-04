5edbd94b24460147d6ce24487f62314e59e8f3fb
belenac <eac1992@gmail.com>
2018-05-04 19:01:52 -0300

    Ejercicio 01 part 1

---
+++ 01/ejercicio01.rb
@@ -0,0 +1,15 @@
+# Utilizando map generar un nuevo arreglo con cada valor aumentado en 1.
+# Utilizando map generar un nuevo arreglo que contenga todos los valores convertidos a float.
+# Utilizando map generar un nuevo arreglo que contenga todos los valores convertidos a string.
+# Utilizando reject descartar todos los elementos menores a 5 en el array.
+# Utilizando select descartar todos los elementos mayores a 5 en el array.
+# Utilizando inject obtener la suma de todos los elementos del array.
+# Utilizando group_by agrupar todos los números por paridad (si son pares, es un grupos, si son impares es otro grupo).
+# Utilizando group_by agrupar todos los números mayores y menores que 6.
+
+a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+b = a.map do |e|
+  e += 1
+end
+
+print b
